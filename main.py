4bee4ca23ff9c55b23f4bbd1734017fd546425ed
Isaac Peterson <isaac.peterson@mottmac.com>
2021-11-20 10:00:49 +1100

    FEAT: Filters actually working now

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ from logconfig.logconfig import setuplogging
 setuplogging()
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 logger.info("test")
 
-logger.debug("debugging stuff")
+# logger.debug("debugging stuff")
 
-loglogger = logging.getLogger('logconfig')
-loglogger.setLevel(logging.INFO)
-loglogger.info("Did anthing run?")
+# loglogger = logging.getLogger('logconfig')
+# loglogger.setLevel(logging.INFO)
+# loglogger.info("Did anthing run?")
 
-# devlogger level is set to DEBUG and the dev logger is configured to write to debug.log
-devlogger = logging.getLogger('dev')
-devlogger.setLevel(logging.DEBUG)
-devlogger.info("devLogger")
+# # devlogger level is set to DEBUG and the dev logger is configured to write to debug.log
+# devlogger = logging.getLogger('dev')
+# devlogger.setLevel(logging.DEBUG)
+# devlogger.info("devLogger")
 
-devlogger.warning("We're just warning up")
-devlogger.critical("Whoa critical error")
+# devlogger.warning("We're just warning up")
+# devlogger.critical("Whoa critical error")
 
-import example1
+# import example1
 
 # print(devlogger.getChild())
