ec21c7d241152665ad3544ea45dfaa35c79bb21a
Isaac Peterson <isaac.peterson@mottmac.com>
2021-11-20 11:09:08 +1100

    FEAT: filters example all G

--- example1.py
+++ example1.py
@@ -1,16 +1,13 @@
 # from logconfig import logconfig # The root logger does not need to be imported each time IF the root logger (i.e logconfig.logconfig) has been called from main
 import logging
-
-print(__name__)
-
 logger = logging.getLogger(__name__)
-# logger.setLevel(logging.INFO)
-# print(logger.parent)
 logger.info("Running Example1.py")
+
+Bigone = 122000
 
 def running():
     # did something
-    logger.info("yeeeaaah. Look at the func")
+    logger.info(f"yeeeaaah. Look at the func with this Bigone: {Bigone}")
     return None
 
 running()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from logconfig.logconfig import setuplogging
 setuplogging()
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO) # example set to DEBUG i.e 
+logger.setLevel(logging.DEBUG) # example set to DEBUG i.e 
 
 logger.debug("debugging stuff")
 logger.debug("Detailed information, typically of interest only when diagnosing problems.")
@@ -18,6 +18,11 @@ logger.warning("An indication that something unexpected happened, or indicative 
 
 logger.error("Erronius")
 logger.error("Due to a more serious problem, the software has not been able to perform some function.")
+
+def random():
+    logger.debug("another thing")
+
+random()
 
 logger.critical("Chaos ensues")
 logger.critical("A serious error, indicating that the program itself may be unable to continue running.")
